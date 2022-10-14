1bbb7598209f8121a1d9a7b733adce351db16a49
Ghostop02 <115125956+Ghostop02@users.noreply.github.com>
2022-10-15 01:42:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -147,6 +147,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
+            btn.append([InlineKeyboardButton(text="🔍ɢᴏᴏɢʟᴇ🔎", url='https://www.google.com/')])
             btn = [[
           InlineKeyboardButton('🔍ɢᴏᴏɢʟᴇ🔎', url='https://www.google.com/')
           ]]
