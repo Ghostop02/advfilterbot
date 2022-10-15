13079013f8718dfcb2ea3d134bbde4d58d4e4bae
Ghostop02 <115125956+Ghostop02@users.noreply.github.com>
2022-10-16 00:00:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ghostop02/advfilterbot.git /EvaMaria
+  git clone https://github.com/Ghostop02/advfilterbot.git /advfilterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
